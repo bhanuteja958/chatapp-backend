0f7a64067274edc72fab1c598fa98114c283c7be
bhanuteja958 <banu.teja958@gmail.com>
2024-09-25 23:32:06 +0530

    feat: Simple server upgrade to websocket with authentication

--- src/common/helper.ts
+++ src/common/helper.ts
@@ -9,3 +9,21 @@ export const createResponse = (
         data,
     };
 };
+
+export const parseCookies = (cookieHeader: string) => {
+    const cookies: any = {};
+
+    if (cookieHeader) {
+        const keyValues = cookieHeader.split(";");
+
+        keyValues.forEach((keyValue: string) => {
+            const [key, value]: string[] = keyValue
+                .split("=")
+                .map((part) => part.trim());
+
+            cookies[key] = value;
+        });
+    }
+
+    return cookies;
+};
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,14 @@ import { pool } from "./connections/sqldb";
 import cors from "cors";
 import userRouter from "./routes/user.routes";
 import cookieParser from "cookie-parser";
+import { WebSocket, WebSocketServer } from "ws";
+import { createServer, IncomingMessage, Server } from "http";
+import internal from "stream";
+import { JwtPayload, verify, VerifyErrors } from "jsonwebtoken";
+import { parseCookies } from "./common/helper";
 
 const app: Express = express();
+const server: Server = createServer(app);
 const PORT: number = parseInt(process.env.PORT) || 3000;
 
 app.use(cors({ origin: "*" }));
@@ -19,6 +25,61 @@ app.use((req: Request, res: Response, next: () => void) => {
 
 app.use("/api/v1/user", userRouter);
 
-app.listen(PORT, () => {
+app.on("upgrade", () => {});
+
+const wss = new WebSocketServer({
+    noServer: true,
+    path: "/chat",
+});
+
+wss.on("connection", (socket: WebSocket, req: IncomingMessage) => {
+    socket.on("message", (message) => {
+        socket.send(`Echo ${message}`);
+    });
+
+    socket.on("close", () => {
+        console.log("connection closed");
+    });
+});
+
+server.on(
+    "upgrade",
+    (req: IncomingMessage, socket: internal.Duplex, head: Buffer) => {
+        if (req.url === "/chat") {
+            const cookies = parseCookies(req.headers["cookie"]);
+            const token = cookies.access_token;
+            if (!token) {
+                socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+                socket.destroy();
+                return;
+            }
+
+            verify(
+                token,
+                process.env.JWT_ACCESS_SECRET,
+                (err: VerifyErrors, decoded: JwtPayload) => {
+                    if (err) {
+                        console.log(err);
+                        socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+                        socket.destroy();
+                        return;
+                    }
+
+                    wss.handleUpgrade(
+                        req,
+                        socket,
+                        head,
+                        (ws: WebSocket, req: IncomingMessage) => {
+                            wss.emit("connection", ws, req);
+                        }
+                    );
+                }
+            );
+        } else {
+            socket.destroy();
+        }
+    }
+);
+server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
